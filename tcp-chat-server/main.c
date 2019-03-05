8433aa08ef0234f51a926b26036094f8a2fe0b4a
gashikwa <benjamin.kwashigah@mail.utoronto.ca>
2019-03-04 20:15:32 -0500

    added print statement :)

---
+++ tcp-chat-server/main.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: kwashiga
+ *
+ * Created on March 4, 2019, 8:12 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("This is a TCP chat client");
+    return (EXIT_SUCCESS);
+}
+
